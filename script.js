0a4a1829ea5d4ef292110d389e990b7ac5f6b1c5
John <johnsief12@gmail.com>
2023-04-01 14:17:39 -0400

    added rainbow button and colors to pad

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const container = document.getElementById('container');
 const slider = document.getElementById("slider");
+const gridValue = document.getElementById("grid-value")
+const rainbow = document.getElementById("rainbow")
+const eraser = document.getElementById("erase")
 
 slider.addEventListener("click", () => { //
     let squares = slider.value;
@@ -22,3 +25,14 @@ slider.addEventListener("click", () => { //
     });
 
 });
+
+rainbow.addEventListener("click", () => {
+    const gridCells = document.querySelectorAll(".square");
+
+    gridCells.forEach(cell => {
+        let color = ["red", "orange", "yellow","green", "blue", "indigo", "violet"];
+            cell.addEventListener("mouseover", () => {
+                cell.style.background = color[Math.floor(Math.random() * color.length)];
+        });
+    });
+})
